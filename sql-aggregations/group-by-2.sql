856eb6e07c24d0194b29d3893cf5f56d12199134
Matthew Cranford <mcranfordmail@gmail.com>
2018-06-20 17:23:52 -0700

    Add more group by 2 practice

--- sql-aggregations/group-by-2.sql
+++ sql-aggregations/group-by-2.sql
@@ -23,3 +23,14 @@ GROUP BY w.channel, s.name
 ORDER BY occurrences DESC;
 
 -- Determine the number of times a particular channel was used in the web_events table for each region. Your final table should have three columns - the region name, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
+SELECT r.name, w.channel, COUNT(s.name) occurrences
+FROM accounts a
+    JOIN web_events w
+    ON a.id = w.account_id
+    JOIN sales_reps s
+    ON a.sales_rep_id = s.id
+    JOIN region r
+    ON s.region_id = r.id
+GROUP BY channel, r.name
+ORDER BY occurrences DESC;
+
