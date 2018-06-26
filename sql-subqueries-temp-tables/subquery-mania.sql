5040f0e10e1aa97e59d4c2e62aba2367aa0bef00
Matthew Cranford <mcranfordmail@gmail.com>
2018-06-25 18:15:05 -0700

    Add more subquery mania practice

--- sql-subqueries-temp-tables/subquery-mania.sql
+++ sql-subqueries-temp-tables/subquery-mania.sql
@@ -29,41 +29,48 @@ ON t3.region_name = t2.region_name AND t3.total_sales = t2.max_sales
 
 
 -- For the region with the largest (sum) of sales total_amt_usd, how many total (count) orders were placed? 
-SELECT
-  (SELECT MAX(t1.total_sales)
-  FROM
-    (SELECT r.name region, SUM(o.total_amt_usd) total_sales
-        FROM accounts a
-        JOIN sales_reps sr
-        ON a.sales_rep_id = sr.id
-        JOIN region r
-        ON sr.region_id = r.id
-        JOIN orders o 
-        ON a.id = o.account_id
-        GROUP BY 1) t1
-   ) t2
-JOIN 
- (SELECT r.name region, SUM(o.total_amt_usd) total_sales
-      FROM accounts a
-      JOIN sales_reps sr
-      ON a.sales_rep_id = sr.id
-      JOIN region r
-      ON sr.region_id = r.id
-      JOIN orders o 
-      ON a.id = o.account_id
-      GROUP BY 1) t3
-ON 
-t3.region = t2.region AND t3.total_sales = t2.total_sales
- 
- 
-
-
-
-
-
+SELECT r.name, COUNT(o.total) total
+FROM accounts a
+JOIN sales_reps sr
+ON a.sales_rep_id = sr.id
+JOIN region r
+ON sr.region_id = r.id
+JOIN orders o 
+ON a.id = o.account_id
+GROUP BY 1
+HAVING  SUM(o.total_amt_usd) =
+(SELECT MAX(t1.total_sales)
+    FROM
+      (SELECT r.name region, SUM(o.total_amt_usd) total_sales
+          FROM accounts a
+          JOIN sales_reps sr
+          ON a.sales_rep_id = sr.id
+          JOIN region r
+          ON sr.region_id = r.id
+          JOIN orders o 
+          ON a.id = o.account_id
+          GROUP BY 1) t1
+     )
 
 -- For the name of the account that purchased the most (in total over their lifetime as a customer) standard_qty paper, how many accounts still had more in total purchases? 
-
+SELECT COUNT(*)
+FROM
+  (SELECT a.name
+  FROM accounts a
+  JOIN orders o 
+  ON o.account_id = a.id
+  GROUP BY 1
+  HAVING SUM(o.total) > 
+    (SELECT total
+    FROM
+      (SELECT a.name, a.id, SUM(o.standard_qty) total_std, SUM(o.total) total
+      FROM accounts a
+      JOIN orders o 
+      ON a.id = o.account_id
+      GROUP BY 1,2
+      ORDER BY 3 DESC
+      LIMIT 1) t1)
+  ) t2;
 
 -- For the customer that spent the most (in total over their lifetime as a customer) total_amt_usd, how many web_events did they have for each channel?
 
