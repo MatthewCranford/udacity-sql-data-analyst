ee4fd1a8718ef467cd05abdae14e8f0d5ecf5a11
Matthew Cranford <mcranfordmail@gmail.com>
2018-07-05 18:33:06 -0600

    Finish left right practice

--- sql-data-cleaning/left-right.sql
+++ sql-data-cleaning/left-right.sql
@@ -14,7 +14,7 @@ ORDER BY 2 DESC
 
 
 -- Use the accounts table and a CASE statement to create two groups: one group of company names that start with a number and a second group of those company names that start with a letter. What proportion of company names start with a letter?
-WITH T1 AS(
+WITH t1 AS(
     SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6','7','8','9') 
                        THEN 1 ELSE 0 END AS num, 
          CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6','7','8','9') 
@@ -27,5 +27,10 @@ FROM t1
 
 
 -- Consider vowels as a, e, i, o, and u. What proportion of company names start with a vowel, and what percent start with anything else?
-SELECT name
-FROM accounts
+WITH T1 AS(
+    SELECT name, CASE WHEN LEFT(LOWER(name), 1) IN ('a','e','i','o','u') THEN 1 ELSE 0 END AS vowel,
+    CASE WHEN LEFT(LOWER(name), 1) IN ('a','e','i','o','u') THEN 0 ELSE 1 END AS consonant
+    FROM accounts)
+
+SELECT sum(vowel) vowels, sum(consonant) consonants
+FROM T1
